4b70ea35f8e106aeeb8882c2d51ed00847be33e1
Franck Fernandez <franck.fernandez@gmail.com>
2022-01-06 15:23:56 +0100

    Create Blog layout

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -6,23 +6,10 @@ const AboutPage = () => {
     <>
       <MetaTags title="About" description="About page" />
 
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <p>
-          Ce site est créé avec pour seule intention de démontrer la puissance
-          créative de Redwood! Oui, c'est très impressionant :D
-        </p>
-        <Link to={routes.home()}>Retour à la page d'accueil</Link>
-      </main>
+      <p>
+        Ce site est créé avec pour seule intention de démontrer la puissance
+        créative de Redwood! Oui, c'est très impressionant :D
+      </p>
     </>
   )
 }
