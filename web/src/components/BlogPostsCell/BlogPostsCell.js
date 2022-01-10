e97831f1a1e22f668fd4266dfc6653581fe85166
Franck Fernandez <franck.fernandez@gmail.com>
2022-01-10 12:41:38 +0100

    yarn rw g cell BlogPosts

---
+++ web/src/components/BlogPostsCell/BlogPostsCell.js
@@ -0,0 +1,34 @@
+export const QUERY = gql`
+  query BlogPostsQuery {
+    posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ posts }) => {
+  return (
+    <ul>
+      {posts.map(({ id, title, body, createdAt }) => (
+        <article key={id}>
+          <header>
+            <h2>{title}</h2>
+          </header>
+          <p>{body}</p>
+          <div>Créé le : {createdAt}</div>
+        </article>
+      ))}
+    </ul>
+  )
+}
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,12 +1,12 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import BlogPostsCell from 'src/components/BlogPostsCell'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-
       <main>Home</main>
+      <BlogPostsCell />
     </>
   )
 }
