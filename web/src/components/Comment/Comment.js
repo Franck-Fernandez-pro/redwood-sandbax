0d702b8d27608c351aa16767e7c21b43ef74327a
Franck Fernandez <franck.fernandez@gmail.com>
2022-01-23 11:47:46 +0100

    Create delete btn on a comment, this btn is reserved to moderator role

--- web/src/components/Comment/Comment.js
+++ web/src/components/Comment/Comment.js
@@ -1,3 +1,5 @@
+import { useAuth } from '@redwoodjs/auth'
+
 const formattedDate = (datetime) => {
   const parsedDate = new Date(datetime)
   const month = parsedDate.toLocaleString('default', { month: 'long' })
@@ -5,8 +7,13 @@ const formattedDate = (datetime) => {
 }
 
 const Comment = ({ comment }) => {
+  const { hasRole } = useAuth()
+  function moderate() {
+    console.log('delete')
+  }
+
   return (
-    <div className="bg-gray-200 p-8 rounded-lg">
+    <div className="relative bg-gray-200 p-8 rounded-lg">
       <header className="flex justify-between">
         <h2 className="font-semibold text-gray-700">{comment.name}</h2>
         <time className="text-xs text-gray-500" dateTime={comment.createdAt}>
@@ -14,6 +21,15 @@ const Comment = ({ comment }) => {
         </time>
       </header>
       <p className="text-sm mt-2">{comment.body}</p>
+      {hasRole('moderator') && (
+        <button
+          className="absolute bottom-2 right-2 bg-red-500 text-xs rounded text-white px-2 py-1"
+          type="button"
+          onClick={moderate}
+        >
+          Delete
+        </button>
+      )}
     </div>
   )
 }
